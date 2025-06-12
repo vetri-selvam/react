18ad8ef21e90f1896f7c85bddc1aadf994ddd5da
vselvamp <vselvamp@griddynamics.com>
2025-06-12 15:28:19 +0530

    FetchGet

---
+++ react-api/src/FetchGet.jsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from "react";
+
+export default function FetchGet() {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Send GET request to the server
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => res.json())
+      .then((data) => {
+        setPosts(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching posts:", error);
+        setLoading(false);
+      });
+  }, []);
+
+  // if (loading) return <p>Loading...</p>;
+
+  return (
+    <div>
+      <h2>📬 Posts from JSONPlaceholder</h2>
+      {posts.slice(0, 5).map((post) => (
+        <div key={post.id}>
+          <h3>{post.title}</h3>
+          <p>{post.body}</p>
+          <hr />
+        </div>
+      ))}
+    </div>
+  );
+}
